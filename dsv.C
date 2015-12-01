ea12618b79f0f181f87ba568e9ff9e1e0997595c
GregorioIII <greg_deleon00@yahoo.com>
2015-12-01 16:21:47 +0100

    Double SV distribution

--- dsv.C
+++ dsv.C
@@ -1,42 +1,39 @@
 #include <vector>
+#include <string>
 #include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <TString.h>
-#include <map>
-#include <TH1.h>
+#include <TH1D.h>
+#include <TMath.h>
 #include <TFile.h>
-#include <TCanvas.h>
-#include <TF1.h>
-#include <TLegend.h>
-#include <TLatex.h>
-#include <TAxis.h>
-#include <TStyle.h>
-#include <TProfile.h>
-#include <TPad.h>
-#include "TEfficiency.h"
-#include <TStyle.h>
-#include "untuplizer.h"
 #include <TClonesArray.h>
 #include <TLorentzVector.h>
- 
-using namespace std;
-void bkgsig(std::string inputFile,char name) {
+#include "untuplizer.h"
+#include "readSample.h"
+
+void DoubleSV(std::string inputFile, char name){
+
+  //read the ntuples (in pcncu)
+
+  std::vector<string> infiles;
+
+  readSample(inputFile, infiles);
   
-  //get TTree from file ...
-  TreeReader data(inputFile.data());
+  TreeReader data(infiles);
+  //TreeReader data(inputFile.data());
+  
+  // Declare the histogram
 
-  TH1F* h_leaddoubleSV=new TH1F("","",40,-1.2,1.2);
-  TH1F* h_subldoubleSV=new TH1F("","",40,-1.2,1.2);
+  TH1D* h_leaddoubleSV=new TH1D("","",40,-1.2,1.2);
+  TH1D* h_subldoubleSV=new TH1D("","",40,-1.2,1.2);
 
-  //Event loop
-  for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
+  // begin of event loop
 
-    if (jEntry % 50000 == 0)
-      fprintf(stderr, "Processing event %lli of %lli\n", jEntry + 1, data.GetEntriesFast());
-    
-    data.GetEntry(jEntry);
-    
+  for( Long64_t ev = 0; ev < data.GetEntriesFast(); ev++ ){
+
+    if( ev % 10000 == 0 )
+      fprintf(stderr, "Processing event %lli of %lli\n", ev + 1, data.GetEntriesFast());
+
+    data.GetEntry(ev);
+
     int nFATJet         = data.GetInt("FATnJet");
     const int nFJets=nFATJet;
     TClonesArray* fatjetP4 = (TClonesArray*) data.GetPtrTObject("FATjetP4");
@@ -44,7 +41,7 @@ void bkgsig(std::string inputFile,char name) {
     Float_t*  fatjetPRmass = data.GetPtrFloat("FATjetPRmass");
     vector<bool>    &passFatJetLooseID = *((vector<bool>*) data.GetPtr("FATjetPassIDLoose"));
     Float_t*  fatjetPRmassL2L3Corr = data.GetPtrFloat("FATjetPRmassL2L3Corr");
- 
+
     int nADDJet         = data.GetInt("ADDnJet");
     const int nAJets=nADDJet;
     TClonesArray* addjetP4 = (TClonesArray*) data.GetPtrTObject("ADDjetP4");
@@ -53,51 +50,54 @@ void bkgsig(std::string inputFile,char name) {
     vector<int> fatjet;
     vector<pair<int,int>> Mjj;
     for(int ij=0; ij<nFJets; ij++) {
-	TLorentzVector* thisJet = (TLorentzVector*)fatjetP4->At(ij);
-	if(thisJet->Pt()<200)continue;
-	if(fabs(thisJet->Eta())>2.4)continue;
-	if(!passFatJetLooseID[ij])continue;
-	if(fatjetPRmassL2L3Corr[ij]<105 || fatjetPRmassL2L3Corr[ij]>135)continue;
-		
-	fatjet.push_back(ij);	
+      TLorentzVector* thisJet = (TLorentzVector*)fatjetP4->At(ij);
+      if(thisJet->Pt()<170)continue;
+      if(fabs(thisJet->Eta())>2.4)continue;
+      if(!passFatJetLooseID[ij])continue;
+      if(fatjetPRmassL2L3Corr[ij]<105 || fatjetPRmassL2L3Corr[ij]>135)continue;
+
+      fatjet.push_back(ij);
     }
 
-       if(fatjet.size()<2)continue;
-    
-       for(unsigned int i=0; i<fatjet.size(); i++) {
-	 for(unsigned int j=0; j<i; j++) {
-	   TLorentzVector* thatJet  = (TLorentzVector*)fatjetP4->At(fatjet[i]);
-	   TLorentzVector* thoseJet = (TLorentzVector*)fatjetP4->At(fatjet[j]);
-	   float dEta = fabs(thatJet->Eta() - thoseJet->Eta());
-	   if(dEta>1.3)continue;
+    if(fatjet.size()<2)continue;
 
-	   Double_t mjj = (*thatJet+*thoseJet).M();
-	   if(mjj<1000)continue; 
-	   Mjj.push_back(make_pair(i,j));
-	   
-	 }
-	}
-       
-       
+    for(unsigned int i=0; i<fatjet.size(); i++) {
+      for(unsigned int j=0; j<i; j++) {
+        int index_that = fatjet[i];
+        int index_those = fatjet[j];
+        TLorentzVector* thatJet  = (TLorentzVector*)fatjetP4->At(index_that);
+        TLorentzVector* thoseJet = (TLorentzVector*)fatjetP4->At(index_those);
+        float dEta = fabs(thatJet->Eta() - thoseJet->Eta());
+        if(dEta>1.3)continue;
+
+        Double_t mjj = (*thatJet+*thoseJet).M();
+        if(mjj<1000)continue;
+        Mjj.push_back(make_pair(index_that,index_those));
+
+      }
+    }
+
     for(unsigned int ae = 0;ae<Mjj.size(); ae++) {
       int aa = Mjj[0].second;
       int ee = Mjj[0].first;
-      TLorentzVector* Jet1 = (TLorentzVector*)fatetP4->At(aa); 
+      TLorentzVector* Jet1 = (TLorentzVector*)fatjetP4->At(aa);
       TLorentzVector* Jet2 = (TLorentzVector*)fatjetP4->At(ee);
       for(int ad=0; ad<nAJets; ad++) {
-	  TLorentzVector* Jet3 = (TLorentzVector*)addjetP4->At(ad);
-	  if(Jet1->DeltaR(*Jet3)<0.1) {
-	  h_leaddoubleSV->Fill(addjet_doubleSV[ad]);
-	  }
-	  if(Jet2->DeltaR(*Jet3)<0.1) {
-	      h_subldoubleSV->Fill(addjet_doubleSV[ad]);
-	    }
-	} 
+        TLorentzVector* Jet3 = (TLorentzVector*)addjetP4->At(ad);
+        if(Jet1->DeltaR(*Jet3)<0.1) {
+          h_leaddoubleSV->Fill(addjet_doubleSV[ad]);                                           
+	}
+        if(Jet2->DeltaR(*Jet3)<0.1) {
+          h_subldoubleSV->Fill(addjet_doubleSV[ad]);                                                                    
+	}
+      }
     }
 
-  }
-  TFile* outfile = new TFile(Form("dsv_%d.root",name),"recreate");
-  h_leaddoubleSV->Write(Form("leaddsv_%d",name));
-  h_subldoubleSV->Write(Form("subldsv_%d",name));
+  } // end of event loop
+  fprintf(stderr, "Processed all events\n");
+
+  TFile* outfile = new TFile(Form("doubleSVBKG_%d.root",name),"recreate");
+  h_leaddoubleSV->Write(Form("dsvBKGlead_%d",name));
+  h_subldoubleSV->Write(Form("dsvBKGsubl_%d",name));
   outfile->Write();
 }
