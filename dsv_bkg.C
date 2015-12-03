e4386a7c8851c723a45576ec4d7e0a4f0ae7d2e5
GregorioIII <greg_deleon00@yahoo.com>
2015-12-03 15:21:56 +0100

    Double SV distribution for background

--- dsv_bkg.C
+++ dsv_bkg.C
@@ -9,7 +9,7 @@
 #include "untuplizer.h"
 #include "readSample.h"
 
-void DoubleSV(std::string inputFile, char name){
+void DoubleSV(std::string inputFile,char name) {
 
   //read the ntuples (in pcncu)
 
@@ -22,6 +22,9 @@ void DoubleSV(std::string inputFile, char name){
   
   // Declare the histogram
 
+  Long64_t nTotal=0;
+  Long64_t nPass[20]={0};
+
   TH1D* h_leaddoubleSV=new TH1D("","",40,-1.2,1.2);
   TH1D* h_subldoubleSV=new TH1D("","",40,-1.2,1.2);
 
@@ -33,6 +36,7 @@ void DoubleSV(std::string inputFile, char name){
       fprintf(stderr, "Processing event %lli of %lli\n", ev + 1, data.GetEntriesFast());
 
     data.GetEntry(ev);
+    nTotal++;
 
     int nFATJet         = data.GetInt("FATnJet");
     const int nFJets=nFATJet;
@@ -48,6 +52,7 @@ void DoubleSV(std::string inputFile, char name){
     Float_t*  addjet_doubleSV = data.GetPtrFloat("ADDjet_DoubleSV");
 
     vector<int> fatjet;
+    int fjetselection= 0;
     vector<pair<int,int>> Mjj;
     for(int ij=0; ij<nFJets; ij++) {
       TLorentzVector* thisJet = (TLorentzVector*)fatjetP4->At(ij);
@@ -57,8 +62,10 @@ void DoubleSV(std::string inputFile, char name){
       if(fatjetPRmassL2L3Corr[ij]<105 || fatjetPRmassL2L3Corr[ij]>135)continue;
 
       fatjet.push_back(ij);
+      fjetselection++;
     }
 
+    if(fjetselection>=2)nPass[0]++;
     if(fatjet.size()<2)continue;
 
     for(unsigned int i=0; i<fatjet.size(); i++) {
@@ -73,7 +80,7 @@ void DoubleSV(std::string inputFile, char name){
         Double_t mjj = (*thatJet+*thoseJet).M();
         if(mjj<1000)continue;
         Mjj.push_back(make_pair(index_that,index_those));
-
+	nPass[1]++;
       }
     }
 
@@ -85,10 +92,12 @@ void DoubleSV(std::string inputFile, char name){
       for(int ad=0; ad<nAJets; ad++) {
         TLorentzVector* Jet3 = (TLorentzVector*)addjetP4->At(ad);
         if(Jet1->DeltaR(*Jet3)<0.1) {
-          h_leaddoubleSV->Fill(addjet_doubleSV[ad]);                                           
+          h_leaddoubleSV->Fill(addjet_doubleSV[ad]);
+	  nPass[2]++;                                           
 	}
         if(Jet2->DeltaR(*Jet3)<0.1) {
           h_subldoubleSV->Fill(addjet_doubleSV[ad]);                                                                    
+	  nPass[3]++;
 	}
       }
     }
@@ -96,8 +105,29 @@ void DoubleSV(std::string inputFile, char name){
   } // end of event loop
   fprintf(stderr, "Processed all events\n");
 
+  std::cout << "nTotal    = " << nTotal << std::endl;
+  for(int i=0;i<20;i++) 
+    if(nPass[i]>0)
+      std::cout << "nPass[" << i << "]= " << nPass[i] << std::endl;
+
+  TH1D* h_CutFlow = new TH1D(" ", "" ,4,0,4);
+     
+  char* cut_name[4] = {"FATJet","Mjj","LeadDSV","SublDSV"};  
+
+  for(int i=1;i<=4;i++){ // i is the index of column of cut flow plot
+    if(i==1) {h_CutFlow->SetBinContent(i,nPass[0]); }
+    else {h_CutFlow->SetBinContent(i,nPass[i-1]); }
+    h_CutFlow->GetXaxis()->SetBinLabel( i , cut_name[i-1] );
+  }
+
+  TFile* outfile = new TFile(Form("counting_%d.root",name),"recreate");
+  h_CutFlow->Write(Form("count_%d",name));
+  outfile->Write();
+
+  /*
   TFile* outfile = new TFile(Form("doubleSVBKG_%d.root",name),"recreate");
   h_leaddoubleSV->Write(Form("dsvBKGlead_%d",name));
   h_subldoubleSV->Write(Form("dsvBKGsubl_%d",name));
   outfile->Write();
+  */
 }
