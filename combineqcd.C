e9adb146f89476b77689d760d1b63400c3b8bb6e
Gregorio <greg_deleon00@yahoo.com>
2016-05-20 11:18:36 +0200

    HT bin

--- combineqcd.C
+++ combineqcd.C
@@ -204,7 +204,6 @@ void qcd() {
   h_DelEta->Add(h_DelEta12,norm12);
 
 
-
   TFile* outfile = new TFile("qcd_luminized.root","recreate");
   h_leadDSV->Write("leadDSV");
   h_sublDSV->Write("sublDSV");
@@ -217,4 +216,102 @@ void qcd() {
   h_Mjj->Write("Mjj");
   h_DelEta->Write("DelEta");
   outfile->Write();
+}
+
+void QCD_HT() {
+
+  TFile* file[5];
+
+  //file[0]  = TFile::Open("HT100to200.root");
+  //file[1]  = TFile::Open("HT200to300.root");
+  //file[2]  = TFile::Open("HT300to500.root");
+  file[0]  = TFile::Open("HT500to700.root");
+  file[1]  = TFile::Open("HT700to1000.root");
+  file[2]  = TFile::Open("HT1000to1500.root");
+  file[3]  = TFile::Open("HT1500to2000.root");
+  file[4]  = TFile::Open("HT2000toInf.root");
+
+  TH1F *h_leadDSV[5] = {NULL};
+  TH1F *h_sublDSV[5] = {NULL};
+  TH1F *h_leadPt[5]  = {NULL};
+  TH1F *h_sublPt[5]  = {NULL};
+  TH1F *h_leadPR[5]  = {NULL};
+  TH1F *h_sublPR[5]  = {NULL};
+  TH1F *h_leadEta[5] = {NULL};
+  TH1F *h_sublEta[5] = {NULL};
+  TH1F *h_Mjj[5]     = {NULL};
+  TH1F *h_DelEta[5]  = {NULL};
+
+
+  for(int i=0;i<5;i++) {
+
+    h_leadDSV[i]  = (TH1F*)(file[i]->Get("leadDSV"));
+    h_sublDSV[i]  = (TH1F*)(file[i]->Get("sublDSV"));
+    h_leadPt[i]   = (TH1F*)(file[i]->Get("leadPt"));
+    h_sublPt[i]   = (TH1F*)(file[i]->Get("sublPt"));
+    h_leadPR[i]   = (TH1F*)(file[i]->Get("leadPR"));
+    h_sublPR[i]   = (TH1F*)(file[i]->Get("sublPR"));
+    h_leadEta[i]  = (TH1F*)(file[i]->Get("leadEta"));
+    h_sublEta[i]  = (TH1F*)(file[i]->Get("sublEta"));
+    h_Mjj[i]      = (TH1F*)(file[i]->Get("Mjj"));
+    h_DelEta[i]   = (TH1F*)(file[i]->Get("DelEta"));
+  }
+
+  Double_t norm[5];
+  //norm[0] = 2683.7*(27990000+-4073)/8209500;
+  //norm[1] = 2683.7*(1712000+-376.3)/18784379;
+  //norm[2] = 2683.7*(347700+-74.81)/16909004;
+  norm[0] = 2683.7*(32100+-7)/14882885;
+  norm[1] = 2683.7*(6831+-1.7)/14050788;
+  norm[2] = 2683.7*(1207+-0.5)/14613039;
+  norm[3] = 2683.7*(119.9+-0.06)/3097583;
+  norm[4] = 2683.7*(25.24+-0.02)/5911831;
+
+  TH1F* hleadDSV = (TH1F*)h_leadDSV[1]->Clone("hleadDSV");
+  hleadDSV->Reset();
+  TH1F* hsublDSV = (TH1F*)h_sublDSV[1]->Clone("hsublDSV");
+  hsublDSV->Reset();
+  TH1F* hleadPt = (TH1F*)h_leadPt[1]->Clone("hleadPt");
+  hleadPt->Reset();
+  TH1F* hsublPt = (TH1F*)h_sublPt[1]->Clone("hsublPt");
+  hsublPt->Reset();
+  TH1F* hleadPR = (TH1F*)h_leadPR[1]->Clone("hleadPR");
+  hleadPR->Reset();
+  TH1F* hsublPR = (TH1F*)h_sublPR[1]->Clone("hsublPR");
+  hsublPR->Reset();
+  TH1F* hleadEta = (TH1F*)h_leadEta[1]->Clone("hleadEta");
+  hleadEta->Reset();
+  TH1F* hsublEta = (TH1F*)h_sublEta[1]->Clone("hsublEta");
+  hsublEta->Reset();
+  TH1F* hMjj = (TH1F*)h_Mjj[1]->Clone("hMjj");
+  hMjj->Reset();
+  TH1F* hDelEta = (TH1F*)h_DelEta[1]->Clone("hDelEta");
+  hDelEta->Reset();
+
+  for(int x=0;x<5;x++) {
+    hleadDSV->Add(h_leadDSV[x],norm[x]);
+    hsublDSV->Add(h_sublDSV[x],norm[x]);
+    hleadPt->Add(h_leadPt[x],norm[x]);
+    hsublPt->Add(h_sublPt[x],norm[x]);
+    hleadPR->Add(h_leadPR[x],norm[x]);
+    hsublPR->Add(h_sublPR[x],norm[x]);
+    hleadEta->Add(h_leadEta[x],norm[x]);
+    hsublEta->Add(h_sublEta[x],norm[x]);
+    hMjj->Add(h_Mjj[x],norm[x]);
+    hDelEta->Add(h_DelEta[x],norm[x]);
+  }
+
+  TFile* outfile = new TFile("QCD_HT.root","recreate");
+  hleadDSV->Write("leadDSV");
+  hsublDSV->Write("sublDSV");
+  hleadPt->Write("leadPt");
+  hsublPt->Write("sublPt");
+  hleadPR->Write("leadPR");
+  hsublPR->Write("sublPR");
+  hleadEta->Write("leadEta");
+  hsublEta->Write("sublEta");
+  hMjj->Write("Mjj");
+  hDelEta->Write("DelEta");
+  outfile->Write();
+
 }
